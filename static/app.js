4fd0a86d9bda227f069e987ee674cfc0ecafeca3
user <mail@mail.local>
2026-05-12 12:53:35 +0100

    Show 'None' on All button when all filters are disabled

--- static/app.js
+++ static/app.js
@@ -37,10 +37,7 @@ function fotoDecider() {
     },
 
     toggleAllFilters() {
-      const allEnabled = this.markFilters[1] && this.markFilters[2] && this.markFilters[3] && 
-                        this.markFilters[4] && this.markFilters[5] && this.markFilters[6] && 
-                        this.markFilters[7] && this.markFilters[8] && this.markFilters[9] &&
-                        this.markFilters.showUnmarked;
+      const allEnabled = this.areAllFiltersEnabled();
       if (allEnabled) {
         for (let i = 1; i <= 9; i++) {
           this.markFilters[i] = false;
@@ -53,6 +50,13 @@ function fotoDecider() {
         this.markFilters.showUnmarked = true;
       }
       this.updateFilteredIndices();
+    },
+
+    areAllFiltersEnabled() {
+      return this.markFilters[1] && this.markFilters[2] && this.markFilters[3] && 
+             this.markFilters[4] && this.markFilters[5] && this.markFilters[6] && 
+             this.markFilters[7] && this.markFilters[8] && this.markFilters[9] &&
+             this.markFilters.showUnmarked;
     },
 
     updateFilters() {
